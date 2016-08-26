01ab2abd832ea295a506e3029099846dd0f2316f
augustaa <august.aalde@outlook.com>
2016-08-26 14:42:35 +0200

    Adda Unity phys test project

---
+++ 2D phys testing/Assets/charPhys.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class charPhys : MonoBehaviour {
+    float x;
+    float hspd;
+    float vspd;
+    [SerializeField]
+    private float gravity = 9.81f;
+
+	// Use this for initialization
+	void Start () {
+        hspd = 0;
+        vspd = 0;
+        transform.Rotate(0, 0, 45f);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        vspd += 0.0001f;
+        transform.Translate(vspd,hspd,0,Space.Self);
+        transform.Rotate(0, 0, 3f);
+    }
+}
