8ad7d73cea1ad7af0e2e0d94c6f0acfe02fde111
augustaa <august.aalde@outlook.com>
2016-08-26 15:33:22 +0200

    Ole kødda med koden

--- 2D phys testing/Assets/charPhys.cs
+++ 2D phys testing/Assets/charPhys.cs
@@ -3,22 +3,45 @@ using System.Collections;
 
 public class charPhys : MonoBehaviour {
     float x;
-    float hspd;
-    float vspd;
-    [SerializeField]
-    private float gravity = 9.81f;
+    float y;
+    float hSpeed;
+    float vSpeed;
+    //[SerializeField]
+    private float gravity = -9.81f;
 
-	// Use this for initialization
-	void Start () {
-        hspd = 0;
-        vspd = 0;
+    // Use this for initialization
+    void Start()
+    {
+        hSpeed = 0;
+        vSpeed = 0;
+        x = transform.position.x;
+        y = transform.position.y;
+
         transform.Rotate(0, 0, 45f);
+
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        vspd += 0.0001f;
-        transform.Translate(vspd,hspd,0,Space.Self);
-        transform.Rotate(0, 0, 3f);
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        updateVelocity();
+        updatePosition();
+        //vSpeed -= gravity*Time.deltaTime;
+        //x += hSpeed;
+        //y += vSpeed;
+        //x = Mathf.Sin(y);
+        
+        //transform.Rotate(0, 0, 3f);
+        //transform.position = new Vector3(x,y,0);
+    }
+
+    void updateVelocity()
+    {
+        vSpeed += gravity * Time.deltaTime;
+    }
+
+    void updatePosition()
+    {
+        transform.position += (new Vector3(hSpeed, vSpeed, 0) * Time.deltaTime);
     }
 }
