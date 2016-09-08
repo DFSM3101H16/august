f2bd1bae82f7481c6455d9d013a8e6b5915355dc
augustaa <august.aalde@outlook.com>
2016-09-08 09:35:38 +0200

    Endret på koden til luftmotstand, tror den funker nå

--- 2D phys testing/Assets/charPhys.cs
+++ 2D phys testing/Assets/charPhys.cs
@@ -4,22 +4,28 @@ using System.Collections;
 public class charPhys : MonoBehaviour {
 
     Collider2D myCollider;
+    gameControl control;
 
     public Vector3 acceleration;
     public Vector3 velocity;
     public Vector3 gravity;
     public Vector3 force;
+    public Vector3 totalForce;
     public float mass = 2f;
     public  Vector3 drag;
 
     // Use this for initialization
     void Start()
     {
+        control = GameObject.Find("GameControl").GetComponent<gameControl>();
+        
+        
         myCollider = GetComponent<Collider2D>();
         acceleration = new Vector3(0,0,0);
         velocity = new Vector3(0,0,0);
         gravity = new Vector3(0,-9.81f,0);
         force = new Vector3(0,0,0);
+        totalForce = new Vector3(0, 0, 0);
         drag = new Vector3(0,0,0);
 
 
@@ -29,29 +35,52 @@ public class charPhys : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        //force = gravity * mass;
-        updateDrag();
+        updateForce();
         updateAcceleration();
         updateVelocity();
-
         updatePosition();
-
         updateCollision();
+        updateDrag();
 
+
+        checkInput();
+        
+
+
+    }
+
+    void checkInput()
+    {
         if (Input.GetKeyDown("space"))
         {
             gravity *= -1;
         }
+
+        if (Input.GetKeyDown("up"))
+        {
+            control.wind = new Vector2(control.wind.x, control.wind.y + 2);
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            control.wind = new Vector2(control.wind.x, control.wind.y - 2);
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            control.wind = new Vector2(control.wind.x - 2, control.wind.y);
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            control.wind = new Vector2(control.wind.x + 2, control.wind.y);
+        }
+
         if (Input.GetKey("a"))
         {
-            force = new Vector3(-10, force.y,force.z);
+            force = new Vector3(-10, force.y, force.z);
         }
         if (Input.GetKey("d"))
         {
             force = new Vector3(10, force.y, force.z);
         }
-
-
     }
 
     void updateCollision()
@@ -65,13 +94,22 @@ public class charPhys : MonoBehaviour {
 
     void updateDrag()
     {
-        drag = new Vector3(-Mathf.Sign(velocity.x) * ((1.05f * Mathf.Pow(velocity.x, 2)) / 2), -Mathf.Sign(velocity.y)*((1.05f * Mathf.Pow(velocity.y, 2)) / 2), 0);
+
+        float deltaVx = velocity.x - control.wind.x;
+        float deltaVy = velocity.y - control.wind.y;
+        drag = new Vector3(-(Mathf.Sign(deltaVx))*((1.2f * 1.05f * Mathf.Pow(deltaVx, 2)) / 2), -(Mathf.Sign(deltaVy))*(1.2f *1.05f * Mathf.Pow(deltaVy, 2) / 2), 0);
+    }
+
+    void updateForce()
+    {
+
+        totalForce = (gravity*mass) + drag + force;
+        force *= 0;
     }
 
     void updateAcceleration()
     {
-        acceleration = (((gravity*mass) + drag + force)/mass);
-        force *= 0;
+        acceleration = (totalForce/mass);
     }
 
     void updateVelocity()
